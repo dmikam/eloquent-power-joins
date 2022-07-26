6b7600b6efb280a92777ac4736f8ff2c804a1d09
Dima Kamynin <dima@ociohoteles.com>
2022-07-26 12:53:35 +0200

    run additional tests

--- tests/JoinRelationshipWithJoinTypesTest.php
+++ tests/JoinRelationshipWithJoinTypesTest.php
@@ -14,7 +14,7 @@ class JoinRelationshipWithJoinTypesTest extends TestCase
 
 
     /** @test */
-    public function test_apply_condition_to_join_1()
+    public function test_apply_condition_to_join_2()
     {
         $queryBuilder = User::query()->joinRelationship('posts', function ($join) {
             $join->where('posts.published', true);
